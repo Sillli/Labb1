d7ace6b39b36f7a9406471b3a4a3e33df55ad669
midiakeshto <keshtomidia@gmail.com>
2020-01-27 13:25:28 +0100

    car

---
+++ Assignment 1/Car.java
@@ -0,0 +1,85 @@
+
+import java.awt.*;
+
+public class Car {
+
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+
+/*
+    public final static double trimFactor = 1.25;
+    public boolean turboOn;
+    */
+
+
+    public void setNrDoors(int nrDoors) {
+        this.nrDoors = nrDoors;
+    }
+
+    public void setEnginePower(double enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public void setCurrentSpeed(double currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public Car() {
+
+    }
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public double speedFactor(){
+        return enginePower * 0.01;
+    }
+
+    public void incrementSpeed(double amount){
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+    }
+
+    /*public void setTurboOff(){
+        turboOn = false;
+    }*/
+
+}
