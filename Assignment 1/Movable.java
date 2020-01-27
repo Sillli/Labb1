aba68e50df8a8a2769cb6c5ae700b486c3d735d7
Sillli <mirris97@live.se>
2020-01-27 14:28:18 +0100

    jajsas

--- Assignment 1/Movable.java
+++ Assignment 1/Movable.java
@@ -1,3 +1,61 @@
 public class Movable {
 
+    private double dirX;
+    private double dirY;
+    private double posX;
+    private double posY;
+
+    public Movable(double dirX, double dirY, double posX, double posY) {
+        this.dirX = dirX;
+        this.dirY = dirY;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public void move() {
+        setPosX(getPosX() + dirX);
+        setPosY(getPosY() + dirY);
+    }
+
+    public void turnLeft(){
+
+    }
+
+    public void turnRight(){
+
+    }
+
+    public void setDirX(double dirX) {
+        this.dirX = dirX;
+    }
+
+    public void setDirY(double dirY) {
+        this.dirY = dirY;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
+
+    public double getDirX() {
+        return dirX;
+    }
+
+    public double getDirY() {
+        return dirY;
+    }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+
 }
