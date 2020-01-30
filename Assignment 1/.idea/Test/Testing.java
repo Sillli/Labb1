c3e9a75e6033a5d822e469b355b0f9302f83d7f6
RavCarl <carlsandbakken@gmail.com>
2020-01-30 16:38:26 +0100

    Test made for StartEngine , StopEnging , Gas() and Brake() methods

--- Assignment 1/.idea/Test/Testing.java
+++ Assignment 1/.idea/Test/Testing.java
@@ -17,22 +17,49 @@ public class Testing {
     public void testSpeedFactor(){
         assertTrue(volvo.speedFactor()==1.25);
     }
-
     @Test
     public void testincrementSpeedVolvo(){
         volvo.setCurrentSpeed(10);
         volvo.incrementSpeed(10);
+        System.out.println(volvo.getCurrentSpeed());
         assertTrue(volvo.getCurrentSpeed()==22.5);
-
     }
-
-    @Test
-    public void testDecrementSpeedVolvo (){
+   @Test
+    public void testDecrementSpeedVolvo(){
         volvo.setCurrentSpeed(10);
         volvo.decrementSpeed(10);
-        assertTrue(volvo.getCurrentSpeed()== 0);
+       System.out.println(volvo.getCurrentSpeed());
+        assertTrue(volvo.getCurrentSpeed()==(0));
+   }
 
+   @Test
+    public void testStartEngineCar(){
+        volvo.startEngine();
+       System.out.println(volvo.getCurrentSpeed());
+        assertTrue(volvo.getCurrentSpeed()>0);
+   }
+   @Test
+    public void testStopEngineCar(){
+        volvo.startEngine();
+        volvo.stopEngine();
+       System.out.println(volvo.getCurrentSpeed());
+        assertTrue(volvo.getCurrentSpeed()==0);
+   }
+   @Test
+    public void testGasCarVolvo(){
+      volvo.gas(10);
+       System.out.println(volvo.getCurrentSpeed());
+       assertTrue(volvo.getCurrentSpeed()==12.5);
+   }
+    @Test
+    public void testBrakeCarVolvo(){
+        volvo.setCurrentSpeed(10);
+        volvo.brake(10);
+        System.out.println(volvo.getCurrentSpeed());
+        assertTrue(volvo.getCurrentSpeed()==0);
     }
+
+
 
 
 
