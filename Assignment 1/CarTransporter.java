99f853acf574860c8e46c0e95c3865667305e7c4
midiakeshto <keshtomidia@gmail.com>
2020-02-03 16:32:41 +0100

    Javadoc in Scania class

---
+++ Assignment 1/CarTransporter.java
@@ -0,0 +1,10 @@
+public class CarTransporter extends Car {
+
+
+
+
+    @Override
+    public double speedFactor() {
+        return 0;
+    }
+}
--- Assignment 1/Scania.java
+++ Assignment 1/Scania.java
@@ -5,14 +5,23 @@ public class Scania extends Car {
     private double flatbedAngle;
     private final static double trimFactor = 1.10;
 
+    /**
+     *constructor of the class Scania, sets it unique attributes
+     */
     public Scania() {
         setEnginePower(400);
         setModelName("Scania");
         setNrDoors(2);
 
-        this.flatbedAngle = 0;
+        this.flatbedAngle = flatbedAngle;
     }
 
+    /**
+     * method that tips the flatbed, has a few conditions
+     * if the scania is moving this can not happen
+     * if the scania already has the flatbed up it can move the flatbed further
+     *
+     */
     public void tipFlatbed() {
         if (getCurrentSpeed() != 0) {
             //DO NOTHING
@@ -21,6 +30,9 @@ public class Scania extends Car {
         }
     }
 
+    /**
+     * Method that decides if the flatbed can be lowerd
+     */
     public void lowerFlatbed() {
         if (getCurrentSpeed() != 0) {
             //DO NOTHING
@@ -29,19 +41,35 @@ public class Scania extends Car {
         }
     }
 
+    /**
+     * getting the angle of the flatbed, 0 or 70
+     * @return flatbedAngle, shows if the angle is up or down
+     */
     public double getFlatbedAngle() {
         return flatbedAngle;
     }
 
+    /**
+     * setting the angle of the flatbed, either to  0 or 70
+     * @param flatbedAngle the angle the flatbed is gonna get
+     */
     public void setFlatbedAngle(double flatbedAngle) {
         this.flatbedAngle = flatbedAngle;
     }
 
+    /**
+     *
+     * @return the speedfactor of the scania-truck
+     */
     @Override
     public double speedFactor() {
         return getEnginePower()*0.01*trimFactor;
     }
 
+    /**
+     * Checks if the flatbed is up, and if it is the scania can not move and a "error message" prints out
+     * if tthe flatbed is down the car can move
+     */
     @Override
     public void move() {
         if (getFlatbedAngle() != 0) {
