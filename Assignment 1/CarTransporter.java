2b2a7309b592c7940f25dc59ffe6c5ecc159aa2c
Sillli <mirris97@live.se>
2020-02-04 12:09:39 +0100

    startade smått, ändra hur ni vill

--- Assignment 1/CarTransporter.java
+++ Assignment 1/CarTransporter.java
@@ -1,10 +1,30 @@
-public class CarTransporter extends Car {
+public class CarTransporter extends Scania {
 
+    private boolean rampUp;
+    private int maxCarsLoaded = 6;
 
+    public CarTransporter() {
+        setModelName("Mercedes");
+        rampUp = true;
+    }
 
 
     @Override
     public double speedFactor() {
         return 0;
     }
+
+    public void setRampUp() {
+        rampUp = true;
+    }
+
+    public void setRampDown() {
+        if (getCurrentSpeed() != 0) {
+            //do nothing
+        } else {
+            rampUp = false;
+        }
+    }
+
+
 }
