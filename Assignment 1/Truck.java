342a520d8a6765281363b1b1e434fa246e7cb087
Sillli <mirris97@live.se>
2020-02-05 14:21:27 +0100

    added stuff

--- Assignment 1/Truck.java
+++ Assignment 1/Truck.java
@@ -19,15 +19,17 @@ public abstract class Truck extends Movable {
      */
     @Override
     public void move() {
-        if (getFlatbedAngle() != 0) {
+        if (flatbed.getFlatbedAngle() != 0) {
             setCurrentSpeed(0);
             System.out.println("Flatbed is up");
-        } else if (getFlatbedAngle() == 0) {
-            super.move();
+        } else if (flatbed.getFlatbedAngle() == 0) {
+            if (getDirX() != 0) {
+                setPosX((int) (getPosX() + getDirX() * getCurrentSpeed()));
+            } else if (getDirY() != 0) {
+                setPosY((int) (getPosY() + getDirY() * getCurrentSpeed()));
+            }
         }
     }
-
-
 
 
 }
