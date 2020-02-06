8f78431c38b9f2f5092d42b349165a73d017f343
midiakeshto <keshtomidia@gmail.com>
2020-02-06 17:12:59 +0100

    engine in ferry

--- Assignment 1/Ferry.java
+++ Assignment 1/Ferry.java
@@ -1,12 +1,19 @@
+import java.awt.*;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class Ferry implements IStoreCars {
 
+    private  Caross caross;
+    private Engine engine;
+
     private final int maxCars;
 
-    public Ferry (int maxCars){
+    public Ferry (int maxCars, int nrdoors, Color color, double enginepower, double trimfactor, boolean carturbo){
         this.maxCars = maxCars;
+        this.caross = new Caross(nrdoors,color);
+        this.engine = new Engine(enginepower, trimfactor, carturbo);
+
     }
 
     Deque<Car> cars = new LinkedList<>();
@@ -20,7 +27,7 @@ public class Ferry implements IStoreCars {
 }
     @Override
     public void offLoadingCar() {
-        while (cars.size()<0){
+        while (cars.size()>0){
             cars.pop();
         }
 
